8a3d4808df7b5b486cb12b3281c0de5efc2cab33
Nick Shvelidze <captain@pirrate.me>
2013-12-22 00:45:41 +0400

    Fancy shit

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
-$(function(){
-  "use strict";
+"use strict";
 
-  $("#form-where").typeahead({
-    name: "where",
-    local: ["Georgia", "USA", "India", "Mcdolans"]
-  });
-
-  $("#form-hackathon").typeahead({
-    name: "hackathon",
-    local: ["Hack@CST", "HackMIT", "DevFest Tbilisi", "Weekend MiniHack"]
-  });
-
-  $("#main-form").submit(function() {
-  })
+$("#form-where").typeahead({
+  name: "where",
+  local: ["Georgia", "USA", "India", "Mcdolans"]
 });
+
+$("#form-hackathon").typeahead({
+  name: "hackathon",
+  local: ["Hack@CST", "HackMIT", "DevFest Tbilisi", "Weekend MiniHack"]
+});
+
+if("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(function(location) {
+    console.log(location)
+  })
+}
