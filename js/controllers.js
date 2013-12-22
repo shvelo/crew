3dda371bc61351c3401a3cf499c39b84bc35fe42
Nick Shvelidze <captain@pirrate.me>
2013-12-22 15:38:48 +0400

    Switch to Ember

--- js/controllers.js
+++
@@ -1,9 +0,0 @@
-var hackathonApp = angular.module('hackathonApp', []);
- 
-hackathonApp.controller('hackathonCtrl', function ($scope) {
-  $scope.hackathons = data.hackathons;
-
-  $scope.hackathons.featured = data.hackathons.map(function(hackathon) {
-  	if(hackathon.featured) return hackathon;
-  });
-});
